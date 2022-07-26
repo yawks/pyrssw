8fd683b394d3f9c639f1474e91bf4ef1c2de59c7
mat <4396128+yawks@users.noreply.github.com>
2022-07-26 16:40:08 +0200

    use cloudscraper for izismile to avoid blocked queries

--- pyrssw_handlers/izismile_handler.py
+++ pyrssw_handlers/izismile_handler.py
@@ -2,8 +2,9 @@ import re
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
 import utils.dom_utils
-from typing import Dict, List, Optional, Tuple, cast
+from typing import Dict, List, Tuple, cast
 import requests
+import cloudscraper
 from lxml import etree
 import datetime
 import time
@@ -12,6 +13,15 @@ from pyrssw_handlers.abstract_pyrssw_request_handler import \
     PyRSSWRequestHandler
 
 
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Content-Type": "text/html; charset=utf-8",
+    "Accept-Language": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4",
+    "Cache-Control": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+    "Pragma": "no-cache"
+}
 class IzismileHandler(PyRSSWRequestHandler):
 
     def get_original_website(self) -> str:
@@ -25,7 +35,7 @@ class IzismileHandler(PyRSSWRequestHandler):
         return "https://izismile.com/favicon.ico"
 
     def get_feed(self, parameters: dict, session: requests.Session) -> str:
-        feed = session.get(url=self.get_rss_url(), headers={}).text
+        feed = session.get(url=self.get_rss_url(), headers=HEADERS).text
 
         # avoid php errors at the beginning when any
         feed = feed[feed.find("<"):]
@@ -39,7 +49,7 @@ class IzismileHandler(PyRSSWRequestHandler):
         spicy_links: List[str] = re.findall(
             "<link>([^<]*izispicy[^<]*highlights[^<]*)</link>", str(feed))
         for spicy_link in spicy_links:
-            page = session.get(url=spicy_link, headers={}).text
+            page = session.get(url=spicy_link, headers=HEADERS).text
             dom = etree.HTML(page)
             for link in dom.xpath("//p/a[contains(@href, 'https://izispicy.com')]"):
                 spans = link.xpath(".//span")
@@ -60,6 +70,7 @@ class IzismileHandler(PyRSSWRequestHandler):
         return feed
 
     def get_content(self, url: str, parameters: dict, session: requests.Session) -> PyRSSWContent:
+        self.scraper = cloudscraper.create_scraper()
         urls: List[str] = [url, "%s/page,1,%s" % ("/".join(url.split("/")[:-1]), url.split("/")[-1])]
         nb_pages = 1
         content, url_next_page, comments, cpt_comments = self._get_content(
@@ -95,8 +106,8 @@ class IzismileHandler(PyRSSWRequestHandler):
 
     def _get_content(self, url: str, session: requests.Session, with_title: bool = False, cpt_comments: int = 0) -> Tuple[str, str, str, int]:
         url_next_page = ""
-        page = self._get_page_from_url(url, session)
-        dom = etree.HTML(page.text)
+        text = self._get_page_from_url(url, session)
+        dom = etree.HTML(text)
         title = "" if not with_title else utils.dom_utils.get_content(dom, [
                                                                       "//h1"])
         comments = ""
@@ -170,13 +181,12 @@ class IzismileHandler(PyRSSWRequestHandler):
 
         return content, url_next_page, comments, cpt
 
-    def _get_page_from_url(self, url, session: requests.Session):
-        page = session.get(url=url)
-        if page.text.find("You do not have access to the site.") > -1:
+    def _get_page_from_url(self, url, session: requests.Session) -> str:
+        text = self.scraper.get(url).text
+        if text.find("You do not have access to the site.") > -1:
             time.sleep(0.1)
-            page = session.get(url=url, headers={
-                'User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'})
-        return page
+            text = session.get(url=url, headers=HEADERS).text
+        return text
 
     def _clean_content(self, c):
         content = c.replace("<div class=\"break\"/>", "")
